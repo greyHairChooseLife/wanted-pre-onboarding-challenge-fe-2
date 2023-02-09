23c5e9bdc056fec265f9949244f2e60efa9d0417
sy <greyhairchooselife@gmail.com>
2023-02-09 20:00:12 +0900

    Assignment 2

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+interface todo {
+	id: number
+	desc: string
+	isDone: boolean
+	category: string
+	tags?: string[]
+}
+
+
+function createTodo() {}
+
+function readTodo() {}
+
+function updateTodo() {}
+
+function deleteTodo() {}
+
+
+function isEmptyTodo() {}
+
+function alertRule() {}
